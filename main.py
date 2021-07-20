acf0b6c46e8c2ec29565e190ca2ea82c799b8461
Prakash Divy <prakashdivyy@gmail.com>
2021-07-21 01:45:14 +0700

    Handle if today date not found

--- main.py
+++ main.py
@@ -8,18 +8,29 @@ def show_nr_4(ticker):
     search_result = investpy.search_quotes(text=ticker, products=['stocks'],
                                            countries=['united states'], n_results=1)
 
+    today_date = datetime.utcnow().date()
+
     recent_data = search_result.retrieve_recent_data()
 
-    five_days_data = recent_data.tail(5)
+    try:
+        recent_data.loc[today_date.strftime("%Y-%m-%d")]
+        last_day = today_date - timedelta(days=1)
 
-    four_days_data = five_days_data.head(4)
+        five_days_data = recent_data.tail(5)
+        four_days_data = five_days_data.head(4)
+        return calculate_nr_4(four_days_data, last_day)
+    except:
+        last_day = list(recent_data.tail(1).index)[0].to_pydatetime().date()
+        four_days_data = recent_data.tail(4)
+        return calculate_nr_4(four_days_data, last_day)
+
+
+def calculate_nr_4(four_days_data, last_day):
     four_days_data['Narrow'] = abs(
         four_days_data['High'] - four_days_data['Low'])
 
     lowest_narrow = four_days_data.nsmallest(1, 'Narrow')
     lowest_date = list(lowest_narrow.index)[0].to_pydatetime().date()
-
-    last_day = datetime.utcnow().date() - timedelta(days=1)
 
     is_equal_date = last_day == lowest_date
     return is_equal_date
